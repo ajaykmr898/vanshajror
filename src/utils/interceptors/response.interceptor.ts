3eeb5c9233a4b74ae05cbfcd4e680d20ef5b3902
Ajay Kumar <ajay.ce.kumar@avanade.com>
2024-06-26 12:26:20 +0200

    adding interceptor and filter

---
+++ src/utils/interceptors/response.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Response<T> {
+  status: string;
+  data: T;
+  timestamp: string;
+}
+
+@Injectable()
+export class ResponseFormatInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        status: 'success',
+        data: data,
+        timestamp: new Date().toISOString(),
+      })),
+    );
+  }
+}
