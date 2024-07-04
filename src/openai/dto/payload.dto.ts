12a208688dd50eb90e248b6d93f1f156e0d0a580
Ajay Kumar <ajay.ce.kumar@avanade.com>
2024-07-04 12:33:41 +0200

    adding payload def

---
+++ src/openai/dto/payload.dto.ts
@@ -0,0 +1,7 @@
+// compareAnswersDto.ts
+
+export class CompareAnswersDto {
+  questions: Record<string, string>;
+  person_1_answers: Record<string, string>;
+  person_2_answers: Record<string, string>;
+}
